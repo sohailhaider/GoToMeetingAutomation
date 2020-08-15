84d2934dba5c714edad9010bb12faca2a207a8cf
Sohail Haider <sohailh343@gmail.com>
2020-08-15 14:05:46 +0500

    chat title verified as well

--- page-models/messages.page.js
+++ page-models/messages.page.js
@@ -2,16 +2,28 @@ import { Selector, t } from 'testcafe';
 
 class MessagesPage {
     constructor() {
-        this.chatsBtn = Selector("a[title='Chats']");
-        this.room = Selector(".room");
+        this.chatsBtn = Selector("[title='Chats']");
+        this.room = Selector("span[data-testid='room-name']");
+        this.msgInput = Selector("div[role='textbox']");
+        this.sendMsgBtn = Selector('[data-testid="message-submit-button"]');
+        this.chatTitle = Selector('.contact-details');
     }
 
     openChatRoomWith(name){
         return t
             .click(this.chatsBtn)
             .click(this.room.withText(name))
-            .wait(5000);
+            .expect(this.chatTitle.textContent).eql(name);
     }
+
+    verifyMsgBtnAndSendMsg(msg) {
+        return t
+            .expect(this.sendMsgBtn.hasAttribute('disabled')).ok()
+            .typeText(this.msgInput, msg)
+            .expect(this.sendMsgBtn.hasAttribute('disabled')).notOk()
+            .click(this.sendMsgBtn);
+    }
+
 }
 
 export default new MessagesPage();
--- specs.js
+++ specs.js
@@ -2,11 +2,15 @@ import * as testData from './testdata.json';
 import loginPage from './page-models/login.page';
 import messagesPage from './page-models/messages.page';
 
-fixture `Verify Send and Receive P2P messages`
+let randomNumber;
+
+fixture `Verify Send/Receive User message`
     .page `${testData.webUrl}`;
 
-test("Login User", async t  => {
+test("Send User Message", async t  => {
     await loginPage.loginUser(testData.user1.email, testData.user1.password);
-
     await messagesPage.openChatRoomWith(testData.user2.displayName);
-})
+    
+    randomNumber = Math.floor(Math.random() * Math.floor(100));
+    await messagesPage.verifyMsgBtnAndSendMsg(testData.msgPrefix+randomNumber);
+});
