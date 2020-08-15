2fc3136f228432aed2ccec4871f3edbb38f09aa4
Sohail Haider <sohailh343@gmail.com>
2020-08-15 14:28:51 +0500

    Send and Receive Message verified

--- page-models/messages.page.js
+++ page-models/messages.page.js
@@ -7,12 +7,17 @@ class MessagesPage {
         this.msgInput = Selector("div[role='textbox']");
         this.sendMsgBtn = Selector('[data-testid="message-submit-button"]');
         this.chatTitle = Selector('.contact-details');
+
+        this.lastMessageEntry = Selector('.message-entry').nth(-1);
+        this.lastMsgEntryTitle = this.lastMessageEntry.find(".message-from");
+        this.lastMsgEntryText = this.lastMessageEntry.find(".text-message");
+
     }
 
     openChatRoomWith(name){
         return t
             .click(this.chatsBtn)
-            .click(this.room.withText(name))
+            .click(this.room.withText(name), {timeout: 5000})
             .expect(this.chatTitle.textContent).eql(name);
     }
 
@@ -22,6 +27,12 @@ class MessagesPage {
             .typeText(this.msgInput, msg)
             .expect(this.sendMsgBtn.hasAttribute('disabled')).notOk()
             .click(this.sendMsgBtn);
+    }
+
+    verifylastMsgInChat(title, msg) {
+        return t
+            .expect(this.lastMsgEntryTitle.textContent).eql(title)
+            .expect(this.lastMsgEntryText.textContent).contains(msg);
     }
 
 }
--- specs.js
+++ specs.js
@@ -7,10 +7,19 @@ let randomNumber;
 fixture `Verify Send/Receive User message`
     .page `${testData.webUrl}`;
 
-test("Send User Message", async t  => {
-    await loginPage.loginUser(testData.user1.email, testData.user1.password);
-    await messagesPage.openChatRoomWith(testData.user2.displayName);
+    test("Send User Message", async t  => {
+        await loginPage.loginUser(testData.user1.email, testData.user1.password);
+        await messagesPage.openChatRoomWith(testData.user2.displayName);
     
-    randomNumber = Math.floor(Math.random() * Math.floor(100));
-    await messagesPage.verifyMsgBtnAndSendMsg(testData.msgPrefix+randomNumber);
-});
+        randomNumber = Math.floor(Math.random() * Math.floor(100));
+        await messagesPage.verifyMsgBtnAndSendMsg(testData.msgPrefix+randomNumber);
+    
+        await messagesPage.verifylastMsgInChat(testData.user1.displayName, testData.msgPrefix+randomNumber);
+    });
+
+    test("Verify Received User Message", async t  => {
+        await loginPage.loginUser(testData.user2.email, testData.user2.password);
+        await messagesPage.openChatRoomWith(testData.user1.displayName);
+    
+        await messagesPage.verifylastMsgInChat(testData.user1.displayName, testData.msgPrefix+randomNumber);
+    });
